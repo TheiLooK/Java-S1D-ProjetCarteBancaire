50f05c4705773b6d040638e64adf0b75863f61f2
Louka Mellano <louka@mellano.net>
2022-11-01 08:53:33 +0100

    Création de la classe CarteB

--- src/CarteB.java
+++ src/CarteB.java
@@ -1,3 +1,56 @@
 public class CarteB {
+    private float solde;
+    private float découvert;
+    private String codeCarte;
 
+    public CarteB(String code) {
+        solde = 0;
+        découvert = 100;
+        codeCarte = code;
+    }
+
+    public CarteB(float montant, float decouv, String code) {
+        if (montant < 0)
+            solde = 0;
+        else
+            solde = montant;
+
+        if (decouv < 0)
+            découvert = 0;
+        else
+            découvert = decouv;
+
+        codeCarte = code;
+    }
+
+    public boolean etreCodeCorrect(String code) {
+        return codeCarte.equals(code);
+    }
+
+    public void deposer(float montant) {
+        if (montant > 0)
+            solde += montant;
+    }
+
+    public boolean depenser(double prix, String code) {
+        if (etreCodeCorrect(code)) {
+            if ((solde-prix) >= -découvert) {
+                solde -= prix;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String toString() {
+        return "carteB: "+solde+" / -"+découvert;
+    }
+
+    public float getSolde() {
+        return solde;
+    }
+
+    public float getDécouvert() {
+        return découvert;
+    }
 }
