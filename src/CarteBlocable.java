4eedfba7a314973d080554469dac82e1c7d7c7ec
Louka Mellano <louka@mellano.net>
2022-11-08 09:46:00 +0100

    Carte blocable

--- src/CarteBlocable.java
+++ src/CarteBlocable.java
@@ -1,13 +1,15 @@
 // Pour fonctionner, la classe CarteBlocable doit d'abord avoir un attribut booléen bloquée, qui indique si la carte est bloquée ou non.
+// Ensuite, il lui faut un attribut nbEssais qui indique le nombre d'essais restants pour entrer le bon code.
 // Elle doit aussi avoir une méthode bloquer() qui permet de bloquer la carte et donc met l'attribut bloqué à true.
-// La méthode depenser() doit vérifier si la carte est bloquée ou non.
+// La méthode depenser() doit vérifier si la carte est bloquée ou non et compter le nombre de fois qu'on se trompe.
 // Il faudrait également ajouter une méthode de qui renvoi si la carte est bloqué est non.
 
 public class CarteBlocable {
     private float solde;
     private float découvert;
     private String codeCarte;
-    private boolean bloquée = true;
+    private boolean bloquée = false;
+    int nbEssais = 3;
 
     public CarteBlocable(String code) {
         this.solde = 0;
@@ -39,10 +41,20 @@ public class CarteBlocable {
     }
 
     public boolean depenser(double prix, String code) {
-        if (etreCodeCorrect(code) && !bloquée) {
-            if ((this.solde-prix) >= -this.découvert) {
-                this.solde -= prix;
-                return true;
+        if (!bloquée) {
+            if (etreCodeCorrect(code)) {
+                nbEssais = 3;
+                if ((this.solde - prix) >= -this.découvert) {
+                    this.solde -= prix;
+                    return true;
+                }
+            }
+
+            else {
+                nbEssais--;
+                if (nbEssais == 0) {
+                    this.bloquée = true;
+                }
             }
         }
         return false;
@@ -58,20 +70,5 @@ public class CarteBlocable {
 
     public float getDécouvert() {
         return this.découvert;
-    }
-
-    // Méthodes carte bloquée
-    public String bloquer() {
-        if (this.bloquée == true) {
-            return "La carte est déjà bloquée";
-        }
-        else {
-            this.bloquée = true;
-            return "La carte est maintenant bloquée";
-        }
-    }
-
-    public boolean estBLoquée() {
-        return this.bloquée;
     }
 }
